1e3c3216ae3de0423801706b5df21042f37bcfc0
Abu Motaleb <abumotaleb99@gmail.com>
2023-02-06 22:31:00 +0600

    type conversion

---
+++ basics-js/typeConversion.js
@@ -0,0 +1,17 @@
+var mangoPrice = 250;
+var tangerinePrice = "200";
+var totalPrice = mangoPrice + tangerinePrice;
+console.log(totalPrice);
+// Convert tangerine price string to number
+totalPrice = mangoPrice + parseInt(tangerinePrice);
+console.log(totalPrice);
+
+var gpa = "4.41";
+// Convert string to float
+var gpa = parseFloat(gpa);
+console.log(gpa);
+
+var num = 20;
+console.log(num);
+// Convert number to string
+console.log(num.toString());

f7615f25d6d969f406032906a007a7a57df040ee
Abu Motaleb <abumotaleb99@gmail.com>
2023-02-06 22:06:29 +0600

    operators

---
+++ basics-js/operators.js
@@ -0,0 +1,57 @@
+/*
+Arithmetic Operators
++, -, *, /, %, ++, --
+*/
+var num1 = 20;
+var num2 = 10;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+
+/*
+Incremental
+Pre Incremental - Post Incremental
+*/
+// console.log(++num1);
+// console.log(num1++);
+// console.log(num1);
+
+/*
+Assignment Operators
+=, +=, -=, *=, /=
+*/
+var num1 = 20;
+var num2 = 30;
+var num3 = 40;
+var num4 = 30;
+var num5 = 10;
+var num6 = 10;
+var num7 = 20;
+var num8 = 12;
+
+// num1 += num2;
+// console.log(num1);
+
+// num3 -= num4;
+// console.log(num3);
+
+// num5 *= num6;
+// console.log(num5);
+
+// num7 /= num8;
+// console.log(num7);
+
+/*
+Comparison Operators
+===, !==, <, >, <=, >=	
+*/
+console.log(5 === "5"); // false
+console.log(5 === 5); // true
+console.log(10 !== "10"); // true
+console.log(10 < 6); // false
+console.log(10 > 6); //true
+console.log(3 <= 2); // false
+console.log(3 >= 2); // true
